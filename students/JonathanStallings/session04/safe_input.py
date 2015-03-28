8f5b6abbeabb22fa969c18238e2527c34546e534
Jonathan Stallings <jonathanstallings@outlook.com>
2015-03-28 12:48:31 -0700

    Complete safe_input wrapper functino.

---
+++ students/JonathanStallings/session04/safe_input.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+
+def safe_input(prompt):
+    """Return 'None' rather than EOF or KeyboardInterrupt exceptions"""
+    try:
+        raw_input(prompt)
+    except EOFError or KeyboardInterrupt:
+        return 'None'
+
