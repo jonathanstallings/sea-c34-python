7da3bf275426770a00ef009ab7307a56e900efe2
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-02 22:54:00 -0700

    Complete through Section 3.

--- students/JonathanStallings/session05/dict_comp.py
+++ students/JonathanStallings/session05/dict_comp.py
@@ -9,7 +9,7 @@ food_prefs = {
 
 
 def section01():
-    """Complete the task for seciton 1."""
+    """Complete the task for Section 1."""
     print(
         "{name} is from {city}, and he likes {cake} cake, {fruit} fruit, "
         "{salad} salad, and {pasta} pasta."
@@ -17,5 +17,26 @@ def section01():
     )
 
 
+def section02():
+    """Complete the task for Section 2."""
+    key_num = range(16)
+    val_hex = [hex(v) for v in key_num]
+    the_dict = dict([(k, v) for k, v in zip(key_num, val_hex)])
+    print(the_dict)
+
+
+def section03():
+    """Complete the task for Section 3."""
+    the_dict = {k: hex(k) for k in range(16)}
+    print(the_dict)
+
+
+def section04():
+    """Complete the task for Section 4."""
+    
+
 if __name__ == '__main__':
     section01()
+    section02()
+    section03()
+
