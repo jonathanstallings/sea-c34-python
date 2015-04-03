dce3e70cd74cd19177e8bab3fb6c84783df7da22
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-02 23:29:23 -0700

    Complete Section 5.

--- students/JonathanStallings/session05/dict_comp.py
+++ students/JonathanStallings/session05/dict_comp.py
@@ -33,10 +33,41 @@ def section03():
 
 def section04():
     """Complete the task for Section 4."""
-    
+    the_dict = {k: v.count('a') for k, v in food_prefs.items()}
+    print(the_dict)
+
+
+def section05():
+    """Complete the tasks for Section 5."""
+    # Part a
+    s2 = {x for x in range(21) if (x % 2 == 0)}
+    s3 = {x for x in range(21) if (x % 3 == 0)}
+    s4 = {x for x in range(21) if (x % 4 == 0)}
+    print(s2)
+    print(s3)
+    print(s4)
+
+    # Part b
+    sets = s2, s3, s4 = (set(), set(), set())
+    for i, set_x in zip((2, 3, 4), sets):
+        for j in range(21):
+            if j % i == 0:
+                set_x.add(j)
+    print(s2)
+    print(s3)
+    print(s4)
+
+    # Part c
+    s2, s3, s4 = [{x for x in range(21) if x % y == 0} for y in range(2, 5)]
+    print(s2)
+    print(s3)
+    print(s4)
+
 
 if __name__ == '__main__':
     section01()
     section02()
     section03()
+    section04()
+    section05()
 
