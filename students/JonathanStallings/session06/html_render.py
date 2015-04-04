1ea169055b090151cf3434f2ab4601df4684fe4d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-04 16:35:37 -0700

    Complete Step 1 of assignment.

--- students/JonathanStallings/session06/html_render.py
+++ students/JonathanStallings/session06/html_render.py
@@ -7,5 +7,29 @@ Python class example.
 
 # The start of it all:
 # Fill it all in here.
+
+
 class Element(object):
-    pass
+    """An HTML element."""
+    tag = u"html"
+    indent = u"    "
+
+    def __init__(self, content=None):
+        self.content = self.indent + str(self.content) if content else ""
+
+    def append(self, string):
+        """Append string to content."""
+        self.content += (
+            u"{indent}{str}\n".format(indent=self.indent, str=str(string))
+        )
+
+    def render(self, file_out, ind=""):
+        """Render the tag and strings in content."""
+        output = (
+            u"{indent}<{tag}>\n"
+            "{indent}{content}"
+            "{indent}</{tag}>"
+            .format(indent=ind, tag=self.tag, content=self.content)
+        )
+        file_out.write(output)
+
