d5368243d98170acd4ff22e3cb63c198a2714894
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-04 20:21:19 -0700

    Complete Step 2.

--- students/JonathanStallings/session06/html_render.py
+++ students/JonathanStallings/session06/html_render.py
@@ -11,54 +11,56 @@ Python class example.
 
 class Element(object):
     """An HTML element."""
-    header = u""
-    tag = u""
+    tag = u"html"
     indent = u"    "
 
     def __init__(self, content=None):
-        self.content = self.indent + str(self.content) if content else ""
+        self.children = [content] if content else []
 
-    def append(self, string):
-        """Append string to content."""
-        self.content += (
-            u"{indent}{str}".format(indent=self.indent, str=str(string))
+    def append(self, content):
+        """Append content to element."""
+        self.children.append(content)
+
+    def render(self, file_out, ind=u""):
+        """Render the element into HTML."""
+        # output = (
+        #     u"{indent}<{tag}>\n"
+        #     u"{indent}{content}\n"
+        #     u"{indent}</{tag}>"
+        #     .format(
+        #         indent=ind, tag=self.tag, content=self.content
+        #     )
+        # )
+        # file_out.write(output)
+        # print(output)
+        file_out.write(
+            u"{indent}<{tag}>\n".format(indent=ind, tag=self.tag)
+        )
+        for child in self.children:
+            try:
+                child.render(file_out, self.indent + ind)
+            except AttributeError:
+                file_out.write(
+                    u"{indent}{child}\n"
+                    .format(indent=self.indent + ind, child=unicode(child))
+                )
+        file_out.write(
+            u"{indent}</{tag}>\n".format(indent=ind, tag=self.tag)
         )
 
-    def render(self, file_out, ind=""):
-        """Render the tag and strings in content."""
-        output = (
-            u"{indent}{header}\n"
-            "{indent}<{tag}>\n"
-            "{indent}{content}\n"
-            "{indent}</{tag}>"
-            .format(
-                header=self.header, indent=ind,
-                tag=self.tag, content=self.content
-            )
-        )
-        file_out.write(output)
 
-
-class HTML(Element):
-    """The HTML element."""
-    header = u"<!DOCTYPE html>"
+class Html(Element):
+    header = u"<!DOCTYPE html>\n"
     tag = u"html"
 
-    def __init__(self, content=None):
-        self.content = self.indent + str(self.content) if content else ""
+    def render(self, file_out, ind=u""):
+        file_out.write(self.header)
+        Element.render(self, file_out, ind)
 
 
 class Body(Element):
-    """The body element."""
     tag = u"body"
-
-    def __init__(self, content=None):
-        self.content = self.indent + str(self.content) if content else ""
 
 
 class P(Element):
-    """A paragraph element."""
     tag = u"p"
-
-    def __init__(self, content=None):
-        self.content = self.indent + str(self.content) if content else ""
