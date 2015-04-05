33cbcf338d833794c5fde22ff588058a4ff873f9
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-04 22:59:59 -0700

    Remove more PEP 8 annoyances in run_html_render.py.

--- students/JonathanStallings/session06/run_html_render.py
+++ students/JonathanStallings/session06/run_html_render.py
@@ -38,19 +38,19 @@ def render(page, filename):
 # Step 1
 ##########
 
-page = hr.Element()
+# page = hr.Element()
 
-page.append(
-    u"Here is a paragraph of text -- there could be more of them, but "
-    "this is enough  to show that we can do some text"
-)
+# page.append(
+#     u"Here is a paragraph of text -- there could be more of them, but "
+#     "this is enough  to show that we can do some text"
+# )
 
-page.append(
-    u"And here is another piece of text -- "
-    "you should be able to add any number"
-)
+# page.append(
+#     u"And here is another piece of text -- "
+#     "you should be able to add any number"
+# )
 
-render(page, u"test_html_output1.html")
+# render(page, u"test_html_output1.html")
 
 # ## Step 2
 # ##########
@@ -165,7 +165,7 @@ render(page, u"test_html_output1.html")
 # body.append(hr.Hr())
 
 # body.append(u"And this is a ")
-# body.append( hr.A(u"http://google.com", "link") )
+# body.append(hr.A(u"http://google.com", "link"))
 # body.append(u"to google")
 
 # page.append(body)
@@ -184,7 +184,7 @@ render(page, u"test_html_output1.html")
 
 # body = hr.Body()
 
-# body.append( hr.H(2, u"PythonClass - Class 6 example") )
+# body.append(hr.H(2, u"PythonClass - Class 6 example"))
 
 # body.append(hr.P(
 #     u"Here is a paragraph of text -- there could be more of them, "
@@ -196,12 +196,12 @@ render(page, u"test_html_output1.html")
 
 # list = hr.Ul(id=u"TheList", style=u"line-height:200%")
 
-# list.append( hr.Li(u"The first item in a list") )
-# list.append( hr.Li(u"This is the second item", style="color: red") )
+# list.append(hr.Li(u"The first item in a list"))
+# list.append(hr.Li(u"This is the second item", style="color: red"))
 
 # item = hr.Li()
 # item.append(u"And this is a ")
-# item.append( hr.A(u"http://google.com", u"link") )
+# item.append(hr.A(u"http://google.com", u"link"))
 # item.append(u"to google")
 
 # list.append(item)
@@ -215,42 +215,42 @@ render(page, u"test_html_output1.html")
 # # Step 8
 # ########
 
-# page = hr.Html()
+page = hr.Html()
 
 
-# head = hr.Head()
-# head.append( hr.Meta(charset=u"UTF-8") )
-# head.append(hr.Title(u"PythonClass = Revision 1087:"))
+head = hr.Head()
+head.append(hr.Meta(charset=u"UTF-8"))
+head.append(hr.Title(u"PythonClass = Revision 1087:"))
 
-# page.append(head)
+page.append(head)
 
-# body = hr.Body()
+body = hr.Body()
 
-# body.append( hr.H(2, u"PythonClass - Class 6 example") )
+body.append(hr.H(2, u"PythonClass - Class 6 example"))
 
-# body.append(hr.P(
-#     u"Here is a paragraph of text -- there could be more of them, "
-#     "but this is enough  to show that we can do some text",
-#     style=u"text-align: center; font-style: oblique;"
-# ))
+body.append(hr.P(
+    u"Here is a paragraph of text -- there could be more of them, "
+    "but this is enough  to show that we can do some text",
+    style=u"text-align: center; font-style: oblique;"
+))
 
-# body.append(hr.Hr())
+body.append(hr.Hr())
 
-# list = hr.Ul(id=u"TheList", style=u"line-height:200%")
+list = hr.Ul(id=u"TheList", style=u"line-height:200%")
 
-# list.append( hr.Li(u"The first item in a list") )
-# list.append( hr.Li(u"This is the second item", style="color: red") )
+list.append(hr.Li(u"The first item in a list"))
+list.append(hr.Li(u"This is the second item", style="color: red"))
 
-# item = hr.Li()
-# item.append(u"And this is a ")
-# item.append( hr.A(u"http://google.com", "link") )
-# item.append(u"to google")
+item = hr.Li()
+item.append(u"And this is a ")
+item.append(hr.A(u"http://google.com", "link"))
+item.append(u"to google")
 
-# list.append(item)
+list.append(item)
 
-# body.append(list)
+body.append(list)
 
-# page.append(body)
+page.append(body)
 
-# render(page, u"test_html_output8.html")
+render(page, u"test_html_output8.html")
 
