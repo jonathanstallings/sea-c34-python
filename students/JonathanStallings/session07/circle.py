8c9b070b9517dfa2e2d0bd0a316c3a3a0dbcb22d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-08 13:22:17 -0700

    Refactored to remove redundancy.

--- students/JonathanStallings/session07/circle.py
+++ students/JonathanStallings/session07/circle.py
@@ -10,8 +10,6 @@ class Circle(object):
     """Create a circle object."""
     def __init__(self, radius):
         self._radius = radius
-        self._diameter = radius * 2
-        self._area = math.pi * radius ** 2
 
     @classmethod
     def from_diameter(cls, diameter):
@@ -21,25 +19,21 @@ class Circle(object):
     def radius(self):
         return self._radius
 
-    @radius.setter
-    def radius(self, radius):
-        self._radius = radius
-        self._diameter = radius * 2
-        self._area = math.pi * radius ** 2
-
     @property
     def diameter(self):
-        return self._diameter
-
-    @diameter.setter
-    def diameter(self, diameter):
-        self._diameter = diameter
-        self._radius = diameter / 2
-        self._area = math.pi * (diameter/2) ** 2
+        return self._radius * 2
 
     @property
     def area(self):
-        return self._area
+        return math.pi * self._radius ** 2
+
+    @radius.setter
+    def radius(self, radius):
+        self._radius = radius
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self._radius = diameter / 2
 
     def __str__(self):
         return (
@@ -60,11 +54,10 @@ class Circle(object):
         return Circle(self._radius * mult)
 
     def __cmp__(self, other):
-        if self._area > other._area:
+        if self.area > other.area:
             return 1
-        elif self._area < other._area:
+        elif self.area < other.area:
             return -1
         else:
             return 0
-
 
