556ffeab61896f52cf9e37f9379e58dd9d676f47
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-08 13:03:50 -0700

    Complete standard tests for circle.py

--- students/JonathanStallings/session07/circle.py
+++ students/JonathanStallings/session07/circle.py
@@ -36,3 +36,31 @@ class Circle(object):
     @property
     def area(self):
         return self._area
+
+    def __str__(self):
+        return (
+            u"Circle with radius: {:.6f}"
+            .format(float(self._radius))
+        )
+
+    def __repr__(self):
+        return(
+            u"Circle({r})"
+            .format(r=int(self._radius))
+        )
+
+    def __add__(self, other):
+        return Circle(self._radius + other._radius)
+
+    def __mul__(self, mult):
+        return Circle(self._radius * mult)
+
+    def __cmp__(self, other):
+        if self._area > other._area:
+            return 1
+        elif self._area < other._area:
+            return -1
+        else:
+            return 0
+
+
