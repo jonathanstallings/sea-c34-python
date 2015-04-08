82d58aabf193b1f4f3f9f71358c585a418e64915
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-07 17:09:31 -0700

    Complete first part of circle tests.

--- students/JonathanStallings/session07/circle.py
+++ students/JonathanStallings/session07/circle.py
@@ -7,4 +7,32 @@ import math
 
 
 class Circle(object):
-    pass
+    """Create a circle object."""
+    def __init__(self, radius):
+        self._radius = radius
+        self._diameter = radius * 2
+        self._area = math.pi * radius ** 2
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, radius):
+        self._radius = radius
+        self._diameter = radius * 2
+        self._area = math.pi * radius ** 2
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self._diameter = diameter
+        self._radius = diameter / 2
+        self._area = math.pi * (diameter/2) ** 2
+
+    @property
+    def area(self):
+        return self._area
