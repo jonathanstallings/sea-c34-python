5e2cf81bb728fc25034d0d42d9e604e3a0fefd84
Jonathan Stallings <jonathanstallings@outlook.com>
2015-04-08 14:46:09 -0700

    Complete 4 of 4 tests -- test_break_me.py

--- students/JonathanStallings/session01/test_break_me.py
+++ students/JonathanStallings/session01/test_break_me.py
@@ -2,6 +2,22 @@ import pytest
 import break_me as br
 
 
-with pytest.raises(NameError):
-    br.name_break()
+def test_name_break():
+    with pytest.raises(NameError):
+        br.name_break()
+
+
+def test_type_break():
+    with pytest.raises(TypeError):
+        br.type_break()
+
+
+def test_syntax_break():
+    with pytest.raises(SyntaxError):
+        br.syntax_break()
+
+
+def test_attribute_break():
+    with pytest.raises(AttributeError):
+        br.attribute_break()
 
